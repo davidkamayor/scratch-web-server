b0900a2968e92c2bc2e5d47502dfaeb7f3f7578d
David Kamayor <davidkamayor@Davids-MacBook-Pro.local>
2024-08-21 13:08:54 -0400

    replace promise api with async functions

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,40 @@
-// Alternate version of 'src/index.ts' using promise API
-
+// redo of 'src/promises.ts' using aysnc/await instead of promises
 import * as net from "net";
 
 console.log("Welcome to my Web Server!\n");
 
-const server = net.createServer();
+const server = net.createServer({
+  pauseOnConnect: true,
+});
 server.on("connection", newConn);
 server.listen({ host: "127.0.0.1", port: 1234 });
 server.on("error", (err: Error) => {
   throw err;
 });
 
-function newConn(socket: net.Socket): void {
+async function newConn(socket: net.Socket): Promise<void> {
   console.log("new connection", socket.remoteAddress, socket.remotePort);
-  socket.write("Welcome new client!\n");
-  socket.on("end", () => {
-    console.log("EOF.");
-  });
-  socket.on("data", (data: Buffer) => {
-    console.log("data", data);
-    socket.write(data);
-    if (data.includes("q")) {
-      console.log("closing");
-      socket.end();
+  try {
+    await serveClient(socket);
+  } catch (exc) {
+    console.error("exception:", exc);
+  } finally {
+    socket.destroy();
+  }
+}
+
+async function serveClient(socket: net.Socket): Promise<void> {
+  const conn: TCPConn = soInit(socket);
+  while (true) {
+    const data = await soRead(conn);
+    if (data.length === 0) {
+      console.log("end connection");
+      break;
     }
-  });
+
+    console.log("data", data);
+    await soWrite(conn, data);
+  }
 }
 
 function soInit(socket: net.Socket): TCPConn {
@@ -34,11 +44,16 @@ function soInit(socket: net.Socket): TCPConn {
     ended: false,
     reader: null,
   };
+  conn.socket.write("Welcome new client!\n");
   socket.on("data", (data: Buffer) => {
     console.assert(conn.reader);
     conn.socket.pause();
     conn.reader!.resolve(data);
     conn.reader = null;
+    if (data.includes("q")) {
+      console.log("closing");
+      conn.socket.end();
+    }
   });
   socket.on("end", () => {
     conn.ended = false;
@@ -61,6 +76,14 @@ function soInit(socket: net.Socket): TCPConn {
 function soRead(conn: TCPConn): Promise<Buffer> {
   console.assert(!conn.reader); //no concurrent calls
   return new Promise((resolve, reject) => {
+    if (conn.err) {
+      reject(conn.err);
+      return;
+    }
+    if (conn.ended) {
+      resolve(Buffer.from(""));
+      return;
+    }
     conn.reader = { resolve: resolve, reject: reject };
     conn.socket.resume();
   });
@@ -72,6 +95,13 @@ function soWrite(conn: TCPConn, data: Buffer): Promise<void> {
       reject(conn.err);
       return;
     }
+    conn.socket.write(data, (err?: Error) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
   });
 }
 
@@ -84,3 +114,15 @@ type TCPConn = {
     reject: (reason: Error) => void;
   };
 };
+
+type DynBuf = {
+  data: Buffer;
+  length: number;
+};
+
+// function soListen(...): TCPListener;
+// function soAccept(listner: TCPListener)''
+
+// type TCPListener = {
+//   socket: net.Socket
+// }
