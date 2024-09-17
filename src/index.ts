0a960efba56d7b28f83e7c8ed90c1e5f9f874d88
David Kamayor <davidkamayor@Davids-MacBook-Pro.local>
2024-09-16 22:30:06 -0400

    implement message echo

--- src/index.ts
+++ src/index.ts
@@ -24,19 +24,49 @@ async function newConn(socket: net.Socket): Promise<void> {
 }
 
 async function serveClient(socket: net.Socket): Promise<void> {
+  // 1. Parse and remove message from incoming byte stream;
+  // 2. Handle the message
+  // 3. Send the response
   const conn: TCPConn = soInit(socket);
+  const buf: DynBuf = { data: Buffer.alloc(0), length: 0 };
   while (true) {
-    const data = await soRead(conn);
-    if (data.length === 0) {
-      console.log("end connection");
-      break;
+    let msg: null | Buffer = cutMessage(buf);
+    if (!msg) {
+      const data = await soRead(conn);
+      bufPush(buf, data);
+      console.log(data);
+      if (data.length === 0) {
+        console.log("end connection");
+        break;
+      }
+      continue;
     }
-
-    console.log("data", data);
-    await soWrite(conn, data);
+    if (msg.equals(Buffer.from("quit\n"))) {
+      await soWrite(conn, Buffer.from("Bye.\n"));
+      socket.destroy();
+      return;
+    } else {
+      const reply = Buffer.concat([Buffer.from("Echo: "), msg]);
+      await soWrite(conn, reply);
+    }
   }
 }
 
+function cutMessage(buf: DynBuf): null | Buffer {
+  // tests if message is completej using '\n'
+  const idx = buf.data.subarray(0, buf.length).indexOf("\n");
+  if (idx < 0) {
+    return null;
+  }
+  const msg = Buffer.from(buf.data.subarray(0, idx + 1));
+  bufPop(buf, idx + 1);
+  return msg;
+}
+
+function bufPop(buf: DynBuf, len: number): void {
+  buf.data.copyWithin(0, len, buf.length);
+  buf.length -= len;
+}
 function soInit(socket: net.Socket): TCPConn {
   const conn: TCPConn = {
     socket: socket,
@@ -44,17 +74,15 @@ function soInit(socket: net.Socket): TCPConn {
     ended: false,
     reader: null,
   };
+
   conn.socket.write("Welcome new client!\n");
   socket.on("data", (data: Buffer) => {
     console.assert(conn.reader);
     conn.socket.pause();
     conn.reader!.resolve(data);
     conn.reader = null;
-    if (data.includes("q")) {
-      console.log("closing");
-      conn.socket.end();
-    }
   });
+
   socket.on("end", () => {
     conn.ended = false;
     if (conn.reader) {
@@ -63,6 +91,7 @@ function soInit(socket: net.Socket): TCPConn {
     }
     0;
   });
+
   socket.on("error", (err: Error) => {
     conn.err = err;
     if (conn.reader) {
@@ -114,6 +143,22 @@ type TCPConn = {
     reject: (reason: Error) => void;
   };
 };
+
+function bufPush(buf: DynBuf, data: Buffer): void {
+  const newLen = buf.length + data.length;
+  if (buf.data.length < newLen) {
+    // grow the capacity by power of 2
+    let cap = Math.max(buf.data.length, 32);
+    while (cap < newLen) {
+      cap *= 2;
+    }
+    const grown = Buffer.alloc(cap);
+    buf.data.copy(grown, 0, 0);
+    buf.data = grown;
+    data.copy(buf.data, buf.length, 0);
+    buf.length = newLen;
+  }
+}
 
 type DynBuf = {
   data: Buffer;
